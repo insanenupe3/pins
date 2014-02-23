fc80f4958ad95135b13ffe94d0152e9b8f16fb93
Corey Robinson <coreyrobinson@listen360.com>
2014-02-22 23:25:01 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pins::Application.config.secret_key_base = '24ccb9a0ed1e9e4654254b644b6ebe13367575e08f6efbc176848a996ddf9b562b46ef777bed24f493829aac4dbb1c8c3bfa6f7c4f36bc1fd042f161df1f04b9'
